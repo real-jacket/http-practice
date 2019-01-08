480729200e3bd377778dd59a4119ec25b2725746
xiaomu-deepin <1762982273@qq.com>
2019-01-08 22:52:25 +0800

    finish demo css

---
+++ index/js/login.js
@@ -0,0 +1,1 @@
+// let getSubmit = document.querySelector('#')
--- node/index.js
+++ node/index.js
@@ -5,18 +5,20 @@ const server = http.createServer(function (request, response) {
     const { method, url,headers } = request
     const userAgent = headers['user-agent']
 
-    let body = []
-    request.on('data', (chunk) => {
-        body.push(chunk)
-    }).on('end', () => {
-        body = Buffer.concat(body).toString()
-    })
+    // let body = []
+    // request.on('data', (chunk) => {
+    //     body.push(chunk)
+    // }).on('end', () => {
+    //     body = Buffer.concat(body).toString()
+    // })
 
-    request.on('error', (err) => {
-        console.error(err.stack)
-    })
+    // request.on('error', (err) => {
+    //     console.error(err.stack)
+    // })
     console.log('HTTP 路径为\n' + url)
     if (method === 'GET' && url === '/login'){
+        console.log(request)
+        
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html; charset=utf-8')
         
@@ -29,4 +31,8 @@ const server = http.createServer(function (request, response) {
     }
 })
 
+if(!port){
+    console.log('请输入端口，好不好嘛!例如：node ./node/index.js 8888');
+    return
+}
 server.listen(port)
