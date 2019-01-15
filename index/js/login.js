523ef2e5443114891227b8603194c3625d8855f3
xiaomu-deepin <1762982273@qq.com>
2019-01-15 22:07:59 +0800

    add stand eslint

--- index/js/login.js
+++ index/js/login.js
@@ -1,1 +1,11 @@
-// let getSubmit = document.querySelector('#')
+let getForm = document.querySelector('.login-form')
+
+getForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  let hash = {}
+  let need = ['name', 'email', 'password']
+  need.forEach((name) => {
+    let value = getForm.elements[name].value
+    console.log(value)
+  })
+})
--- node/index.js
+++ node/index.js
@@ -2,37 +2,42 @@ const http = require('http')
 const port = process.argv[2]
 
 const server = http.createServer(function (request, response) {
-    const { method, url,headers } = request
-    const userAgent = headers['user-agent']
+  const { method, url, headers } = request
+  const userAgent = headers['user-agent']
+  let path
+  if (url.indexOf('?') !== -1) {
+    path = url.split('?')[0]
+  } else {
+    path = url
+  }
 
-    // let body = []
-    // request.on('data', (chunk) => {
-    //     body.push(chunk)
-    // }).on('end', () => {
-    //     body = Buffer.concat(body).toString()
-    // })
+  // let body = []
+  // request.on('data', (chunk) => {
+  //     body.push(chunk)
+  // }).on('end', () => {
+  //     body = Buffer.concat(body).toString()
+  // })
 
-    // request.on('error', (err) => {
-    //     console.error(err.stack)
-    // })
-    console.log('HTTP 路径为\n' + url)
-    if (method === 'GET' && url === '/login'){
-        console.log(request)
-        
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html; charset=utf-8')
-        
-        response.write('<html>')
-        response.write('<body>')
-        response.write('<h1>hello world</h1>')
-        response.write('</body>')
+  // request.on('error', (err) => {
+  //     console.error(err.stack)
+  // })
+  console.log('HTTP 路径为\n' + path)
+  if (method === 'POST' && path === '/login') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-        response.end()
-    }
+    response.write('<html>')
+    response.write('<body>')
+    response.write('<h1>hello world</h1>')
+    response.write('</body>')
+
+    response.end()
+  } else {
+    response.statusCode = 404
+  }
 })
 
-if(!port){
-    console.log('请输入端口，好不好嘛!例如：node ./node/index.js 8888');
-    return
+if (!port) {
+  console.log('请输入端口，好不好嘛!例如：node ./node/index.js 8888')
 }
 server.listen(port)
